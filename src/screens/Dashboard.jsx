6f4643d309da07ebc3372c4b24eb10a5db809ebc
SkyNet <arierhiprecious@gmail.com>
2025-04-06 15:55:04 +0100

    latest

--- src/screens/Dashboard.jsx
+++ src/screens/Dashboard.jsx
@@ -1,8 +1,13 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Dashboard.module.css';
-import { FaHome, FaWallet, FaChartLine, FaCog, FaBell, FaUser} from 'react-icons/fa'; // Updated icon import
+import { FaHome, FaWallet, FaChartLine, FaCog, FaBell, FaUser } from 'react-icons/fa'; // Updated icon import
 import { HiMenu } from 'react-icons/hi'; // Import the HiMenu icon for the mobile menu
+import { FaShoppingCart, FaTag } from 'react-icons/fa'; // Icons for buy, sell, send, and receive
+import { RiMoneyDollarCircleLine } from 'react-icons/ri'; // Buy icon
+import { MdGetApp } from 'react-icons/md'; // Receive icon
+import {  FaPaperPlane } from 'react-icons/fa'; // Sell and Send icons
+
 
 const Dashboard = () => {
     const [isSidebarOpen, setIsSidebarOpen] = useState(false);
@@ -38,81 +43,54 @@ const Dashboard = () => {
 
             <div className={styles.mainSection}>
                 <div className={styles.headerContainer}>
-
                     <div className={styles.mobileHeader}>
-
-                        <HiMenu color='black' size={30} className={styles.hamburger}/>
-                        
-
-                        <FaBell color='black' size={30} className={styles.bell}/>
+                        <HiMenu color='black' size={30} className={styles.hamburger} />
+                        <FaBell color='black' size={30} className={styles.bell} />
                     </div>
-
-
 
                     <div className={styles.title}>
                         <h2>Home</h2>
                     </div>
 
-
                     <div className={styles.buttonContainer}>
-
                         <button className={styles.buysellbutton}>
                             Buy & Sell
-
                         </button>
                         <button className={styles.sendrecievebutton}>
                             Send & Recieve
-
                         </button>
 
                         <button className={styles.notificationbutton}>
                             <FaBell color='rgb(180,180,180)' size={18} />
                             <span>55</span>
-
                         </button>
 
                         <button className={styles.imagebutton}>
                             <FaUser color='rgb(180,180,180)' size={18} />
-
                         </button>
-
-
-
-
                     </div>
-
-
-
                 </div>
-
 
                 <div className={styles.balanceSection}>
                     <div className={styles.balanceCard}>
                         <p className={styles.amount}>$500.00</p>
                         <p className={styles.amounttext}>Your wallet balance</p>
 
-
                         <div className={styles.balanceActionContainer}>
                             <button>
-                                Buy
+                                <RiMoneyDollarCircleLine size={18} /> Buy
                             </button>
                             <button>
-                                Sell
+                                <FaTag size={18} /> Sell
                             </button>
                             <button>
-                                Send 
+                                <FaPaperPlane size={18} /> Send
                             </button>
                             <button>
-                                Receive
+                             <MdGetApp size={18} /> Receive 
                             </button>
-
-
                         </div>
-                    
                     </div>
-
-
-
                 </div>
             </div>
         </div>
